d0173fbb894b62f6622c6ebbf7c717ea40725798
tinku gupta <tinku.gupta4@gmail.com>
2020-09-09 00:27:46 +0530

    second commet

--- script.js
+++ script.js
@@ -18,6 +18,10 @@ window.addEventListener('load', ()=> {
  let long ;
  let lat ;
 
+ let locationtimezone = document.querySelector(".location-timezone");
+ let temperature_description = document.querySelector(".temperature-description");
+ let temperature_degree = document.querySelector(".temperature-degree");
+
 
  if(navigator.geolocation){
 
@@ -25,14 +29,43 @@ window.addEventListener('load', ()=> {
 
         long = position.coords.longitude;
         lat = position.coords.latitude;
-        console.log(position);
+        const proxy = "https://cors-anywhere.herokuapp.com/";
+        const api = `${proxy}https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`
+        
+        fetch(api)
+            .then(response => {
+
+                return response.json();
+
+                
+
+            })
+
+            .then(data =>{
+                console.log(data);
+                // let currenttemp = data.currently.temperature;
+                // let Summary = data.currently.summary;
+                // console.log(Summary);
+
+             
+
+
+                console.log(currenttemp);
+                const {temperature , summary} = data.currently;
+
+                temperature_degree.textContent = currenttemp;
+            
+                
+
+            });
+        
+
 
     });
-     
- }else{
-     h1.textContent = "hey this page is rquered your location "
- }
 
+}
+     
+ 
 
 });
 
